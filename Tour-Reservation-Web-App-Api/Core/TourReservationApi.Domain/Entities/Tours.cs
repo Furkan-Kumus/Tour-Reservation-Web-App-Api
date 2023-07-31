728a52bb6e95299678c012c6226d3acb8d8e76ac
Furkan Kümüş <furkankumus1905@gmail.com>
2023-07-31 08:45:35 +0300

    API created

---
+++ Tour-Reservation-Web-App-Api/Core/TourReservationApi.Domain/Entities/Tours.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourReservationApi.Domain.Entities.Common;
+
+namespace TourReservationApi.Domain.Entities
+{
+    public class Tours : BaseEntity
+    {
+    }
+}
