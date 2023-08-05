088e69ed549ece188173b013b260d95da754098f
Mustafa Berk Alpay <kisacaberk@gmail.com>
2023-08-05 14:21:18 +0300

    application.features.commands.tour updated

---
+++ Tour-Reservation-Web-App-Api/Core/TourReservationApi.Application/Features/Commands/Tour/CreateCompany/CreateCompanyCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourReservationApi.Application.Features.Commands.Tour.CreateFlight;
+using TourReservationApi.Application.Repositories;
+
+namespace TourReservationApi.Application.Features.Commands.Tour.CreateCreateFlight
+{
+    public class CreateCompanyCommandHandler : IRequestHandler<CreateCompanyCommandRequest, CreateCompanyCommandResponse>
+    {
+        readonly IProductWriteRepository _productWriteRepository;
+
+        public CreateCompanyCommandHandler(IProductWriteRepository productWriteRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+        }
+
+        public async Task<CreateCompanyCommandResponse> Handle(CreateCompanyCommandRequest request, CancellationToken cancellationToken)
+        {
+            await _productWriteRepository.AddAsync(new()
+            {
+                FlightCode = request.FlightCode,
+                FlightRegion = request.FlightRegion,
+            });
+            await _productWriteRepository.SaveAsync();
+            return new();
+        }
+    }
+}
